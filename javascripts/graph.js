e8d14a72521a9cadeaf1c21a0f769c3d776225f3
tiemfah <sivanat.sub@gmail.com>
2019-07-12 17:22:33 +0700

    beautify

--- javascripts/graph.js
+++ javascripts/graph.js
@@ -6,6 +6,7 @@ var zipped = []
 
 
 // GET
+
 fetch(url)
   .then(function (response) {
     return response.json();
@@ -18,30 +19,28 @@ fetch(url)
       return [e, values[i]];
     });
     // GRAPH
-google.charts.load('current', {
-  'packages': ['corechart']
-});
-google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {
+      'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-  var data = google.visualization.arrayToDataTable(zipped);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable(zipped);
 
-  var options = {
-    title: "P'Ken performance",
-    hAxis: {
-      title: 'date',
-      titleTextStyle: {
-        color: '#333'
-      }
-    },
-    vAxis: {
-      minValue: 0
+      var options = {
+        title: "P'Ken performance",
+        hAxis: {
+          title: 'date',
+          titleTextStyle: {
+            color: '#333'
+          }
+        },
+        vAxis: {
+          minValue: 0
+        }
+      };
+
+      var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
     }
-  };
-
-  var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-}
   });
-
-
